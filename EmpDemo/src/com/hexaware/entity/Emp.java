3214e1ba5f6a312be76d647ca12c3022cb8a8735
azhar <azharuddinnshaik@gmail.com>
2025-05-03 12:55:00 +0530

    created Emp entity

--- EmpDemo/src/com/hexaware/entity/Emp.java
+++ EmpDemo/src/com/hexaware/entity/Emp.java
@@ -2,9 +2,29 @@ package com.hexaware.entity;
 
 public class Emp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int empId;
+	private String empName;
+	private double empSal;
+	
+	
+	public int getEmpId() {
+		return empId;
 	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(double empSal) {
+		this.empSal = empSal;
+	}
+	
 
 }
